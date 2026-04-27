17869665c327e13a0aa08592793d77d3fdaa0cb6
itswl <imwl@live.com>
2026-04-27 15:34:44 +0800

    fix(api): refactor remaining synchronous database queries to full async SQLAlchemy 2.0 implementations

--- split_adapters.py
+++
@@ -1,5 +0,0 @@
-
-with open('adapters/ecosystem_adapters.py') as f:
-    lines = f.readlines()
-
-# Instead of risky regex on Python AST, we just explain to the user the roadmap and do the safest/highest value thing: The Adapter Refactor.
